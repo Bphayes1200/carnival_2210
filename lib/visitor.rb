70aa9b56c2d76b8c2b20b0a9504603b1e0bb5503
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-14 09:13:38 -0700

    Feat: add initialize method and spending_money method for class Visitor

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,16 @@
 class Visitor
+  attr_reader :name, 
+              :height,
+              :preferences 
 
+  def initialize(name, height, spending_money)
+    @name = name 
+    @height = height 
+    @spending_money = spending_money
+    @preferences = []
+  end 
+
+  def spending_money
+    @spending_money.delete_prefix('$').to_i
+  end 
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Visitor do
     it 'will exist and have attributes' do 
       visitor = Visitor.new('Bruce', 54, '$10')
 
-      expect(visitor).to be_a(visitor)
-      expect(visitor.name).to_eq("Bruce")
+      expect(visitor).to be_a(Visitor)
+      expect(visitor.name).to eq("Bruce")
       expect(visitor.height).to eq(54)
       expect(visitor.spending_money).to eq(10)
       expect(visitor.preferences).to eq([])
