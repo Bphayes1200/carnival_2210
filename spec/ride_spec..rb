3dbfd58ff693651a6d8680199655dc6652eef9dd
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-14 09:43:35 -0700

    Test : add test for board rider method for class Ride

--- spec/ride_spec..rb
+++ spec/ride_spec..rb
@@ -13,4 +13,22 @@ RSpec.describe Ride do
       expect(ride1.total_revenue).to eq(0)
     end 
   end 
+
+  describe 'board_rider' do 
+    it 'will board a rider' do 
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 36, '$5')
+      visitor1.add_preference(:gentle)
+      visitor2.add_preference(:gentle)
+      ride1.board_rider(visitor1)
+      ride1.board_rider(visitor2)
+      ride1.board_rider(visitor1)
+
+      expect(ride1.rider_log).to be_a(Hash)
+      expect(ride1.rider_log.include?(visitor1)).to eq(true)
+      expect(ride2.rider_log.include?(visitor2)).to eq(true)
+
+    end 
+  end 
 end 
